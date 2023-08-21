58c8f22ef81c256f5ebcd822a4b614087316a46d
kareem22T <78378547+kareem22T@users.noreply.github.com>
2023-08-21 13:09:56 +0300

    sfs

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+   module.exports = {
+     devServer: {
+       proxy: {
+         '/api': {
+           target: 'http://api.egyptgamestore.com',
+           changeOrigin: true,
+           pathRewrite: {
+             '^/api': ''
+           }
+         }
+       }
+     }
+   };
+   
