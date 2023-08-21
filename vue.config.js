c2f8b8c2ac4db226fa315581f57574d2fabe4bd0
kareem22T <78378547+kareem22T@users.noreply.github.com>
2023-08-21 12:40:37 +0300

    try proxy to fix cros origin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/users': {
+        target: 'https://egs-sigma.vercel.app/',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  }
+}
