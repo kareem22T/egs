99a4dcb78eb1eb230c84d96dde9ef18e9e91599c
kareem22T <78378547+kareem22T@users.noreply.github.com>
2023-09-27 07:23:51 +0300

    asdf

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -6,8 +6,8 @@
             </div>
         </div>
         <div class="container">
-            <h1>My Orders</h1>
-            <div class="table_wrapper">
+            <h1 v-if="orders && orders.length > 0">My Orders</h1>
+            <div class="table_wrapper" v-if="orders && orders.length > 0">
                 <table>
                     <thead>
                         <tr>
@@ -20,53 +20,86 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>#12254456</td>
-                            <td><span class="done">Done</span></td>
-                            <td>Master Card</td>
-                            <td>102360 EGP</td>
-                            <td>15/10/2023 - 10:20 PM</td>
-                            <td><button>View Order</button></td>
-                        </tr>
-                        <tr>
-                            <td>#12254456</td>
-                            <td><span class="done">Done</span></td>
-                            <td>Master Card</td>
-                            <td>102360 EGP</td>
-                            <td>15/10/2023 - 10:20 PM</td>
-                            <td><button>View Order</button></td>
-                        </tr>
-                        <tr>
-                            <td>#12254456</td>
-                            <td><span class="canceled">Canceled</span></td>
-                            <td>Master Card</td>
-                            <td>102360 EGP</td>
-                            <td>15/10/2023 - 10:20 PM</td>
+                        <tr v-for="order in orders" :key="order.id" >
+                            <td>#{{order.RefNumber }}</td>
+                            <td><span class="canceled">{{order.status }}</span></td>
+                            <td>{{order.payment_method }}</td>
+                            <td>{{order.total_price.toLocaleString()}} EGP</td>
+                            <td>{{order.created_at }}</td>
                             <td><button>View Order</button></td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+            <h1 v-if="!orders || orders.length == 0"  style="width:100%;margin: 5rem 0px; text-align: center; color: rgb(113, 113, 113);">No orders yet</h1>
         </div>
     </main>
 </template>
 
 <script>
-// global.jQuery = require('jquery');
-// var $ = global.jQuery;
-// window.$ = $;
+global.jQuery = require('jquery');
+var $ = global.jQuery;
+window.$ = $;
 
-// import axios from 'axios';
+import axios from 'axios';
 
 export default {
     name: 'OrdersView',
     data() {
         return {
+            orders: null,
         }
     },
     methods: {
+        async getWishlist() {
+            $('.loader').fadeIn().css('display', 'flex')
+            try {
+                const response = await axios.get(`https://api.egyptgamestore.com/api/users/orders?status=0`,
+                    {
+                        headers: {
+                            "AUTHORIZATION": 'Bearer ' + sessionStorage.getItem('user_token')
+                        },
+                    }
+                );
+                if (response.data.status === true) {
+                    $('.loader').fadeOut()
+                    this.orders = response.data.data
+                } else {
+                    $('.loader').fadeOut()
+                    document.getElementById('errors').innerHTML = ''
+                    $.each(response.data.errors, function (key, value) {
+                        let error = document.createElement('div')
+                        error.classList = 'error'
+                        error.innerHTML = value
+                        document.getElementById('errors').append(error)
+                    });
+                    $('#errors').fadeIn('slow')
+
+                    setTimeout(() => {
+                        $('input').css('outline', 'none')
+                        $('#errors').fadeOut('slow')
+                    }, 3500);
+                }
+
+            } catch (error) {
+                document.getElementById('errors').innerHTML = ''
+                let err = document.createElement('div')
+                err.classList = 'error'
+                err.innerHTML = 'server error try again later'
+                document.getElementById('errors').append(err)
+                $('#errors').fadeIn('slow')
+                $('.loader').fadeOut()
+
+                setTimeout(() => {
+                    $('#errors').fadeOut('slow')
+                }, 3500);
+
+                console.error(error);
+            }
+        },
     },
     created() {
+        this.getWishlist()
     },
     mounted() {
     },
